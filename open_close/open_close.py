942f4cfd02a619b845631cc727c3c4bd41e1aad9
anpopaicoconat <anpopaicoconat@gmail.com>
2020-01-18 18:07:33 +0300

    вывод истории обучения

--- open_close/open_close.py
+++ open_close/open_close.py
@@ -102,3 +102,15 @@ model.evaluate(X_test, Y_test, verbose=1) # Evaluate the trained model on the te
 print('_____________________________start______________________/nконец')
 model.save('open-close.h5')
 print('saved')
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
