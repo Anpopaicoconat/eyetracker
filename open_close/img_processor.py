01bed9687b881236c1c811d4b40396731bf2737b
anpopaicoconat <anpopaicoconat@gmail.com>
2020-01-19 09:02:28 +0300

    тайпл

--- open_close/img_processor.py
+++ open_close/img_processor.py
@@ -18,15 +18,6 @@ def search_f(img):
         landmarks = predictor(gray, face)
         eyes = ((landmarks.part(36).x, np.maximum(landmarks.part(37).y, landmarks.part(38).y), landmarks.part(39).x, np.minimum(landmarks.part(40).y, landmarks.part(41).y)),
                (landmarks.part(42).x, np.maximum(landmarks.part(43).y, landmarks.part(44).y), landmarks.part(45).x, np.minimum(landmarks.part(47).y, landmarks.part(46).y)))
-        xr1 = landmarks.part(36).x
-        yr1 = np.maximum(landmarks.part(37).y, landmarks.part(38).y)
-        xr2 = landmarks.part(39).x
-        yr2 = np.minimum(landmarks.part(40).y, landmarks.part(41).y)
-
-        xl1 = landmarks.part(42).x
-        yl1 = np.maximum(landmarks.part(43).y, landmarks.part(44).y)
-        xl2 = landmarks.part(45).x
-        yl2 = np.minimum(landmarks.part(47).y, landmarks.part(46).y)
 
         # добиваем обрезку для одинакового разрешения
         dif_x = 64-(x2-x1)#s otricatelnimi problema
