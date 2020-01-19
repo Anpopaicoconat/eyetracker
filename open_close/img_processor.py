2d7825620aec561bd0332c617ef0f181fad75dfd
anpopaicoconat <anpopaicoconat@gmail.com>
2020-01-19 17:55:23 +0300

    функции поиска загрузки показа

--- open_close/img_processor.py
+++ open_close/img_processor.py
@@ -10,7 +10,7 @@ import keras
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def search_f(img):
+def search_eye(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
     for face in faces:
@@ -18,13 +18,45 @@ def search_f(img):
         landmarks = predictor(gray, face)
         eyes = ((landmarks.part(36).x, np.maximum(landmarks.part(37).y, landmarks.part(38).y), landmarks.part(39).x, np.minimum(landmarks.part(40).y, landmarks.part(41).y)),
                (landmarks.part(42).x, np.maximum(landmarks.part(43).y, landmarks.part(44).y), landmarks.part(45).x, np.minimum(landmarks.part(47).y, landmarks.part(46).y)))
+        #(xr1 yr1 xr2 yr2) (xl1 yl1 xl2 yl2)
+        for eye in eyes:
+            # добиваем обрезку для одинакового разрешения
+            dif_x = 64-(eye[2]-eye[0])#s otricatelnimi problema
+            dif_y = 64-(eye[3]-eye[1])
+        
+            ad_xl = dif_x//2# нельзя увеличивать больше чем в 2 раза в этом случае произвести ресайз
+            ad_xr = dif_x//2+dif_x%2
+        
+            ad_yl = dif_y//2
+            ad_yr = dif_y//2+dif_y%2
+            print('dif', dif_x, dif_y)
+            eye_img = img[eye[1]-ad_yl: eye[3]+ad_yr, eye[0]-ad_xl: eye[2]+ad_xr]
+            yield eye_img
 
-        # добиваем обрезку для одинакового разрешения
-        dif_x = 64-(x2-x1)#s otricatelnimi problema
-        dif_y = 64-(y2-y1)
-        
-        ad_xl = dif_x//2
-        ad_xr = dif_x//2+dif_x%2
-        
-        ad_yl = dif_y//2
-        ad_yr = dif_y//2+dif_y%2
+def load_images(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        #img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_NEAREST)
+        if img is not None:
+            yield img
+
+def show(img):
+    while(True):
+        k = cv2.waitKey()
+        cv2.imshow("test", img)
+        if k==27:    #выход esc
+            break
+        if k == -1:
+            pass
+        else:
+            print(k)
+
+img_l = list(load_images(r'C:\Users\anpopaicoconat\source\repos\detector\detector\data\open\processed'))
+eyes = []
+for i in img_l:
+    for j in search_eye(i):
+        eyes.append(j)
+
+for img in eyes:
+    show(img)
